59de1281d20e2121aa0938300a6b666b5f298fb2
ferhatucar <ucarferhat3@gmail.com>
2019-07-05 13:20:42 +0200

    adds assets and robot head switcher

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,40 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img src="./assets/build-a-bot-logo.png" alt="" class="logo">
+            Build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-      <HomePage/>
+      <RobotBuilder/>
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
-
   body {
     background: linear-gradient(to bottom, #555, #999) fixed;
   }
-
-
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
   }
-
   main {
     margin: 0 auto;
     padding: 30px;
@@ -35,5 +42,23 @@ export default {
     width: 1024px;
     min-height: 300px;
   }
-
+  header {
+    background-color: #999;
+    width: 1084px;
+    margin: 0 auto;
+  }
+  ul {
+    padding: 3px;
+    display: flex;
+  }
+  .nav-item {
+    display: inline-block;
+    padding: 5px 10px;
+    font-size: 22px;
+    border-right: 1px solid #bbb;
+  }
+  .logo {
+    vertical-align: middle;
+    height: 30px;
+  }
 </style>
